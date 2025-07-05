1addbdb531617b8f149d4dd6a1f29ba91d5bdf4b
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2025-07-05 17:39:47 +0530

    Sorting-1 and Kadane's Algo

--- Java_ND_June_2025/src/Lecture8/KadanesAlgo.java
+++ Java_ND_June_2025/src/Lecture8/KadanesAlgo.java
@@ -10,7 +10,6 @@ public class KadanesAlgo {
 	public int maxSubArray(int[] nums) {
 		int sum =0;
 		int max = Integer.MIN_VALUE;
-		
 		for (int i = 0; i < nums.length; i++) {
 			sum+= nums[i];
 			max = Math.max(max, sum);
@@ -18,7 +17,6 @@ public class KadanesAlgo {
 				sum=0;
 			}
 		}
-		
 		return max;
 	}
 

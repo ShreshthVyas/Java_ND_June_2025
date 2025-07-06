8ecc94132ce205ec5f8e5e76c79d71f454548994
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2025-07-06 14:44:51 +0530

    Sorting and Kadanes

--- Java_ND_June_2025/src/Lecture8/InsertionSort.java
+++ Java_ND_June_2025/src/Lecture8/InsertionSort.java
@@ -19,7 +19,7 @@ public class InsertionSort {
 
 		for (int i = 1; i < arr.length; i++) {
 			int temp =  arr[i];
-			int j = i-1;
+			int j = i-1; 
 			while(j>=0 && arr[j]>temp) {
 				arr[j+1] = arr[j];
 				j--;

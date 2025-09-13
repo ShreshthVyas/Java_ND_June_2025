5aa9cf0d83c938fd362ecbde17b20c59ccb3f03f
SHRESHTH VYAS <shreshth.vyas.2000@gmail.com>
2025-09-13 15:55:18 +0530

    LL-QPS

--- Java_ND_June_2025/src/Lecture27/DetectCycleLeetcode.java
+++ Java_ND_June_2025/src/Lecture27/DetectCycleLeetcode.java
@@ -29,7 +29,6 @@ public class DetectCycleLeetcode {
 		ListNode fast = head;
 		
 		while(fast.next!=null && fast.next.next!=null) {
-			
 			fast = fast.next.next;
 			slow = slow.next;
 			if(slow == fast) {//cycle detected
